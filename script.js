4654c1ff6d2b14acee2d81487526f3226db57339
luisaranda33 <luisluisaranda@gmail.com>
2022-01-28 10:27:50 +0100

    mostrar

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
 // Variable que recoge los elementos con la clase "card"
 var cards = document.querySelectorAll('.card');
 
+
+var cartaGirada = false;
+var front;
+var back;
+
+
 // Funcion flip()
 // Crea un efecto visual de volteo sobre la carta
-function flipCard() {
+function flip() {   
     this.classList.toggle('flip');
+
+    /*
+     mostrarCarta();
+     ocultarCarta();
+    //Funcion mostrar carta
+function mostrarCarta() {
+    if (!cartaGirada()){
+        cartaGirada = true;
+        front = this;
+        console.log({front,back});
+
+        } else {
+             ocultarCarta();
+        }     
 }
+
+//Funcion ocultar carta
+function ocultarCarta() {
+     if (cartaGirada = true()){
+         cartaGirada = false;
+             back = this;
+             console.log({front,back});
+            
+     } else {
+         mostrarCarta();
+
+     }
+   }*/
+
+
+if (!cartaGirada){
+    cartaGirada = true;
+    front = this;
+    console.log({front,back});
+
+} else {
+    cartaGirada = false;
+    back = this;
+    console.log({front,back});
+
+    }
+}
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
