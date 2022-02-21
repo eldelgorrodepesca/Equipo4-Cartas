167722d3c75a7dd4b54fe887fb720e3a4ccb87bb
luisaranda33 <77334518+luisaranda33@users.noreply.github.com>
2022-02-21 03:04:14 +0100

    a

---
+++ Equipo4-Cartas-main/scriptArray.js
@@ -0,0 +1,163 @@
+/*============================== VARIABLES ==============================*/
+
+var cards = document.querySelectorAll(".card"); // recoge los elementos con la clase "card"
+var cartaGirada = false; // indica el estado de la carta
+var front, back; // partes de la carta
+var minutes = 2;
+var seconds = 0;
+var puntos = 0;
+var intentos = 0;
+
+/*================================= FLIP =================================*/
+
+// Crea un efecto visual de volteo sobre la carta y cambia los valores booleanos al hacer click en una carta para asi saber si está girada
+function flipCard() {
+    this.classList.add("flip");
+
+    if (!cartaGirada) {
+        cartaGirada = true;
+        front = this;
+    } else {
+        cartaGirada = false;
+        back = this;
+        comprobar();
+    }
+}
+
+cards.forEach((card) => card.addEventListener("click", flipCard)); // genera el efecto sobre las cartas
+
+/*============================== COMPROBAR CARTAS ==============================*/
+
+//Función que comprueba si las cartas son iguales y llama a bloquear flip en el caso de que sean igual y a ocultar carta en el caso de que sean distintas
+function comprobar() {
+    document.getElementById("intentos").innerHTML = intentos; // imprime los intentos en pantalla
+    if (front.dataset.img == back.dataset.img) {
+        bloquearFlip();
+        juegoFinalizado();
+    } else {
+        intentos++;
+        ocultarCarta();
+    }
+    document.getElementById("puntos").innerHTML = puntos; // imprime los puntos en pantalla
+
+}
+
+/*============================== BLOQUEAR CARTAS ==============================*/
+
+//Función que elimina la posiblidad de hacer click en la carta
+function bloquearFlip() {
+    intentos++;
+    puntos++;
+    front.removeEventListener("click", flipCard);
+    back.removeEventListener("click", flipCard);
+}
+
+/*============================== OCULTAR ==============================*/
+
+//Funcion ocultar carta que elimina el flip despues de 1500 milisegundos
+function ocultarCarta() {
+    setTimeout(() => {
+        intentos++;
+        front.classList.remove("flip");
+        back.classList.remove("flip");
+    }, 1500);
+}
+
+/*============================ MECLAR CARTAS ===========================*/
+
+/*mezcla las cartas ,cada posicion de ellas obtine un numero del 1-8 y se mezclan*/
+function mezclar(){
+  let imagenes = document.querySelectorAll('.imagen')
+
+  var srcs = ['img/1.png','img/2.png','img/3.png','img/4.png','img/5.png','img/6.png','img/7.png','img/8.png','img/1.png','img/2.png','img/3.png','img/4.png','img/5.png','img/6.png','img/7.png','img/8.png'];
+  for(let i=srcs.length-1; i>0; i--){
+      let j = Math.floor(Math.random() * i)
+      let temp = srcs[i]
+      srcs[i] = srcs[j]
+      srcs[j] = temp
+    }
+    
+    for(let i=0; i<imagenes.length; i++){
+      imagenes[i].src = srcs[i]
+    }
+}
+
+/*============================== FINALIZAR ==============================*/
+
+function juegoFinalizado() {
+    // caso en que llegue a conseguir el maximo de puntos
+    if (puntos == 8) {
+        setTimeout(() => {
+            alert("Ha ganado enhorabuena");
+            window.location = "index.html"; // redirige al inicio
+        }, 1500);
+    }
+}
+
+/*============================== REINICIAR ==============================*/
+
+/*La funcion para reiniciar el juego se usa en location.reload() ,luego en el body de board.html esta onload=" mezclar()" para que las mezcle
+despues de refrescar la pagina*/
+
+/*============================== CRONOMETRO ==============================*/
+
+// llama a la funcion del segundero que incluye la del minutero y el stop
+secondHand();
+
+// funcion que explica el funcionamiento del segundero
+function secondHand() {
+    let changeSeconds; // variable que sustituira a los segundos
+
+    if (seconds < 0) {
+        // Caso para cuando los segundos sean menores que 0
+        seconds = 59;
+    }
+
+    if (seconds < 10) {
+        // Caso para cuando los segundos sean menores que 10
+        changeSeconds = "0" + seconds;
+    } else {
+        changeSeconds = seconds;
+    }
+
+    // sustituye a los segundos y los imprime
+    document.getElementById("seconds").innerHTML = changeSeconds;
+    seconds--;
+
+    // llama al minutero y para el juego
+    minuteHand(seconds);
+    stopTime(minutes, seconds);
+}
+
+// funcion que explica el funcionamiento del minutero
+function minuteHand(seconds) {
+    let changeMinutes; // variable que sustituira a los minutos
+
+    if (seconds <= -1 && minutes !== 0) {
+        // caso en el que los minutos y segundos sean menores que 0
+        setTimeout(() => {
+            minutes--;
+        }, 500);
+    }
+
+    if (minutes < 10) {
+        // Caso para cuando los minutes sean menores que 10
+        changeMinutes = "0" + minutes;
+    } else {
+        changeMinutes = minutes;
+    }
+
+    // sustituye e imprime los minutos
+    document.getElementById("minutes").innerHTML = changeMinutes;
+}
+
+setInterval(secondHand, 1000); // intervalo de tiempo del cronometro
+
+// funcion encargada de parar el juego
+function stopTime(minutes, seconds) {
+    // caso en el que se acabe el tiempo
+    if (minutes == 0 && seconds == 0) {
+        alert("Se acabó tu tiempo!!");
+        window.location = "index.html"; // redirige al inicio
+    }
+}
